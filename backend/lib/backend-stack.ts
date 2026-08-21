bc5f29cca724220f492c6c6d06355853b6f589a4
David Perez <a2121145@correo.uia.mx>
2026-08-21 16:15:59 -0600

    Agregar los comandos para agregar nota y enlistarlas

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -53,7 +53,61 @@ export class BackendStack extends cdk.Stack {
       },
     });
 
+    //Union entre el dynamoDB y el GraphQL
     const notesDataSource = api.addDynamoDbDataSource('NotesDataSource', notesTable);
+
+    //El comando para crear una Nota en el DynamoDB
+    notesDataSource.createResolver('CreateNoteResolver', {
+      typeName: 'Mutation',
+      fieldName: 'createNote',
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "version": "2018-05-29",
+          "operation": "PutItem",
+          "key": {
+            "id": $util.dynamodb.toDynamoDBJson($util.autoId())
+          },
+          "attributeValues": {
+            "text": $util.dynamodb.toDynamoDBJson($ctx.args.text),
+            "sentiment": $util.dynamodb.toDynamoDBJson($ctx.args.sentiment),
+            "dateCreated": $util.dynamodb.toDynamoDBJson($util.time.nowISO8601())
+          }
+        }
+      `),
+      responseMappingTemplate: appsync.MappingTemplate.fromString(`$util.toJson($ctx.result)`),
+    });
+
+    //Obtener las notas por bloques
+    notesDataSource.createResolver('GetNotesResolver', {
+      typeName: 'Query',
+      fieldName: 'getNotes',
+      requestMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "version": "2018-05-29",
+          "operation": "Scan",
+          "limit": #if($ctx.args.limit) $ctx.args.limit #else 10 #end,
+          #if($ctx.args.nextToken)
+          "nextToken": "$ctx.args.nextToken",
+          #end
+          #if($ctx.args.sentiment)
+          "filter": {
+            "expression": "sentiment = :sentiment",
+            "expressionValues": {
+              ":sentiment": $util.dynamodb.toDynamoDBJson($ctx.args.sentiment)
+            }
+          }
+          #end
+        }
+      `),
+      responseMappingTemplate: appsync.MappingTemplate.fromString(`
+        {
+          "items": $util.toJson($ctx.result.items),
+          "nextToken": $util.toJson($ctx.result.nextToken),
+          "scannedCount": $ctx.result.scannedCount
+        }
+      `),
+    });
+
 
     //Para revisar como se llaman los nombres y usarlos en el FE
     new cdk.CfnOutput(this, 'GraphQLApiUrl', { value: api.graphqlUrl });
